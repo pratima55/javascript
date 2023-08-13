2dbb4a0146f81962bfbe53a5aa9b7f07d1983ad6
pratima55 <104217964+pratima55@users.noreply.github.com>
2023-08-13 16:24:57 +0000

    6th

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-food = 200
-tipPercentage = 0.2
-tipAmount = food * tipPercentage
-console.log(tipAmount)
 
 //taking imput form user
 //var fruit = prompt('what is your favroit fruit?')
 //console.log(fruit)
 
-food = prompt("how much was the food?")
-tipPercentage = prompt("tip % ?")/100
+food = Number(prompt("how much was the food?"))
+tipPercentage = Number(prompt("tip % ?") / 100)
 tipAmount = food * tipPercentage
-console.log(tipAmount)
+total = food + tipAmount
+console.log('tipAmount', tipAmount)
+console.log('total', total)
+//alert(tipAmount)
