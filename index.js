ef39fdb2bb1b0b7a6b124695cf784b0bad6f516f
Pratima Dhakal <prateemadhakal555@gmail.com>
2023-08-13 22:19:52 +0545

    6th commit

--- index.js
+++
@@ -1,4 +0,0 @@
-food = 200
-tipPercentage = 0.2
-tipAmount= food * tipPercentage
-console.log(tipAmount)
---
+++ script.js
@@ -0,0 +1,8 @@
+
+food = Number(prompt("how much was the food?"))
+tipPercentage = Number(prompt("tip % ?") / 100)
+tipAmount = food * tipPercentage
+total = food + tipAmount
+console.log('tipAmount', tipAmount)
+console.log('total', total)
+//alert(tipAmount)
