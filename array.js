75b84bc0f8e1d8fd1a0fe3ef8107cf6855f6936c
Pratima Dhakal <prateemadhakal555@gmail.com>
2023-08-14 19:21:42 +0545

    array

---
+++ array.js
@@ -0,0 +1,31 @@
+// const fruit = ["apple","mango","grapes","orange","banana"]
+// console.log(fruit)
+
+//adding new object in array
+
+// const fruit = ["apple","mango","grapes","orange","banana"]
+// fruit.push("strawberry","pineapple")
+// console.log(fruit)
+
+//array slice 
+// const fruit = ["apple","mango","grapes","orange","banana"]
+// fruit.push("strawberry","pineapple")
+// console.log(fruit.slice(2,5))
+
+//arrays method (slice, push, indexof,length)
+
+//arrays indexof method
+// const fruit = ["apple","mango","grapes","orange","banana"]
+// fruit.push("strawberry","pineapple")
+// console.log(fruit)
+// console.log(fruit.indexOf("orange"))
+
+//array length method
+// chnaging array
+const fruit = ["apple", "mango", "grapes", "orange", "banana"]
+fruit.push("strawberry", "pineapple")
+fruit[0] = "pear"
+console.log(fruit)
+console.log(fruit.indexOf("orange"))
+console.log(fruit.length)
+console.log(fruit.length - 1)
