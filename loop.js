0f1f0c2d0120915bdbabbfee1413d70f9c3a6abf
Pratima Dhakal <prateemadhakal555@gmail.com>
2023-08-14 23:40:58 +0545

    looping

---
+++ loop.js
@@ -0,0 +1,29 @@
+// const fruit =['apple','mango','banana','pear']
+// console.log(fruit[0])
+// console.log(fruit[1])
+// console.log(fruit[2])
+// console.log(fruit[3])
+
+// const fruit =['apple','mango','banana','pear']
+// for(i=0 ; i<fruit.length; i++)
+// console.log(i,fruit[i])
+
+//example 1
+// const fruit = ['apple', 'mango', 'banana', 'pear']
+// for (const fruits of fruit) {
+//     console.log(fruits)
+// }
+
+//example2
+// const numbers = [1, 2, 3, 4, 5, 6]
+// for (const numeber of numbers) {
+//     console.log(numeber)
+// }
+
+// example3
+const numbers = [1, 2, 3, 4, 5, 6]
+let result = []
+for (const number of numbers) {
+    result.push(number * 2)
+}
+console.log(result)
