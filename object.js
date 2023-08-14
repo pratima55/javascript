f0ede580b6cfdd31b433c0ab899c0c3bd5e87aca
Pratima Dhakal <prateemadhakal555@gmail.com>
2023-08-14 20:31:57 +0545

    objects

---
+++ object.js
@@ -0,0 +1,28 @@
+ //person1
+ const person1 = {
+    name: "pratima",
+    age: 23,
+    number: 1234567890,
+    color: "pink",
+    haircolor: "black",
+
+}
+person1.height= "5ft"
+person1["nationality"]= " Nepali"
+console.log(person1["name"])
+console.log(person1.age)
+console.log(person1.number)
+console.log(person1)
+
+//person2
+const person2 = {
+    name: "aayesha",
+    age: 23,
+    number: 222222222,
+    color: "blue",
+    haircolor: "black",
+
+}
+console.log(person2["name"])
+console.log(person2.age)
+console.log(person2.number)
