a293e9bca90c0d37de82b9e4ab4d769dfb5beb1a
Pratima Dhakal <prateemadhakal555@gmail.com>
2023-08-14 14:13:54 +0545

    Vs code commit

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 
-food = Number(prompt("how much was the food?"))
-tipPercentage = Number(prompt("tip % ?") / 100)
-tipAmount = food * tipPercentage
-total = food + tipAmount
-console.log('tipAmount', tipAmount)
-console.log('total', total)
-//alert(tipAmount)
+// food = Number(prompt("how much was the food?"))
+// tipPercentage = Number(prompt("tip % ?") / 100)
+// tipAmount = food * tipPercentage
+// total = food + tipAmount
+// console.log('tipAmount', tipAmount)
+// console.log('total', total)
+// //alert(tipAmount)
+
+// math=Math.round(4.5)
+// console.log(math)
+
+let weather = 'rain'
+if (weather == 'rain') {
+    console.log('grab umbrella')
+}
+else {
+    console.log('wear sunglasses ')
+}
