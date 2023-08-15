d8d1e21d42b19b70a5cbd1f7d60b1426929466af
Pratima Dhakal <prateemadhakal555@gmail.com>
2023-08-15 15:25:53 +0545

    arrayandboject1

--- arrayandobjectexercise.js
+++ arrayandobjectexercise.js
@@ -8,14 +8,35 @@
 // letterCounter()
 
 
-const howManyLetters = (phrase) =>{
-    let result=0;
-    for(const index in phrase)
-    {
-        console.log(Number(index)+1)
-        result = Number(index)+1
+// const howManyLetters = (phrase) =>{
+//     let result=0;
+//     for(const index in phrase)
+//     {
+//         console.log(Number(index)+1)
+//         result = Number(index)+1
+//     }
+//     return {result: result}
+// }
+// const phrase = prompt("write your phrase")
+//  console.log(howManyLetters(phrase))
+
+//using .length property to calculate length of string
+
+// const howManyLetters = (phrase) => {
+//     return { result: phrase.length }
+// }
+// const phrase = prompt("write your phrase")
+// console.log(howManyLetters(phrase))
+
+
+// sum numebr in array
+const sumArray = (numbers) => {
+    let result = 0;
+    for (const number of numbers) {
+        console.log(number)
+        result = result + number
     }
-    return {result: result}
+    return { result }
 }
-const phrase = prompt("write your phrase")
- console.log(howManyLetters(phrase))
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(sumArray(nums))
